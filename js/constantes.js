088cdbf52e269344bf7cd722ec87a24a47d3893c
Matheus Diniz <mtsdnz@hotmail.com>
2024-06-01 16:31:20 -0300

    Adicionado constantes para pilotos e equipes

---
+++ js/constantes.js
@@ -0,0 +1,154 @@
+const EQUIPES = {
+  porsche: {
+    nome: 'porsche',
+    logo: '../img/Equipes/Porsche_Formula_E.png',
+  },
+  andretti: {
+    nome: 'andretti',
+    logo: '../img/Equipes/Logo-Andretti-Global.webp',
+  },
+  jaguar: {
+    nome: 'jaguar',
+    logo: '../img/Equipes/Logo_da_Jaguar_Racing.png',
+  },
+  maseratti: {
+    nome: 'maseratti',
+    logo: '../img/Equipes/LogoMaserati.png',
+  },
+  mclaren: {
+    nome: 'mclaren',
+    logo: '../img/Equipes/NEOMMclarenLogo.png',
+  },
+  envision: {
+    nome: 'envision',
+    logo: '../img/Equipes/Envision_Racing_logo.png',
+  },
+  mahindra: {
+    nome: 'mahindra',
+    logo: '../img/Equipes/LogoMahindra.png',
+  },
+  nissan: {
+    nome: 'nissan',
+    logo: '../img/Equipes/NissanLogo.png',
+  },
+  abt: {
+    nome: 'abt',
+    logo: '../img/Equipes/Logotipo_da_ABT_CUPRA_Formula_E_Team.png',
+  },
+  ert: {
+    nome: 'ert',
+    logo: '../img/Equipes/ERT_Formula_E_logo.png',
+  },
+  dsPenske: {
+    nome: 'ds-penske',
+    logo: '../img/Equipes/LogoDS_PENSKE.webp.png',
+  },
+};
+
+const PILOTOS = [
+  {
+    nome: 'António Félix Da Costa',
+    equipe: EQUIPES.porsche,
+    foto: '../img/Pilotos/Antonio-Felix-Da-Costa.png',
+  },
+  {
+    nome: 'Nick Cassidy',
+    equipe: EQUIPES.jaguar,
+    foto: '../img/Pilotos/Nick-Cassidy.png',
+  },
+  {
+    nome: 'Oliver Rowland',
+    equipe: EQUIPES.nissan,
+    foto: '../img/Pilotos/Oliver-Rowland.png',
+  },
+  {
+    nome: 'Pascal Wehrlein',
+    equipe: EQUIPES.porsche,
+    foto: '../img/Pilotos/Pascal-Wehrlein.png',
+  },
+  {
+    nome: 'Jake Dennis',
+    equipe: EQUIPES.andretti,
+    foto: '../img/Pilotos/Jake-Dennis.png',
+  },
+  {
+    nome: 'Mitch Evans',
+    equipe: EQUIPES.jaguar,
+    foto: '../img/Pilotos/Mitch-Evans.png',
+  },
+  {
+    nome: 'Jehan Daruvala',
+    equipe: EQUIPES.maseratti,
+    foto: '../img/Pilotos/Jehan-Daruvala.png',
+  },
+  {
+    nome: 'Sam Bird',
+    equipe: EQUIPES.mclaren,
+    foto: '../img/Pilotos/Sam-Bird.png',
+  },
+  {
+    nome: 'Robin Frijns',
+    equipe: EQUIPES.envision,
+    foto: '../img/Pilotos/Robin-Frijns.png',
+  },
+  {
+    nome: 'Lucas di Grassi',
+    equipe: EQUIPES.abt,
+    foto: '../img/Pilotos/Lucas-Di-Grassi.png',
+  },
+  {
+    nome: 'Jake Hughes',
+    equipe: EQUIPES.mclaren,
+    foto: '../img/Pilotos/Jake-Hughes.png',
+  },
+  {
+    nome: 'Sérgio Sette Câmara',
+    equipe: EQUIPES.ert,
+    foto: '../img/Pilotos/Sergio-Sette-Camara.png',
+  },
+  {
+    nome: 'Sébastien Buemi',
+    equipe: EQUIPES.envision,
+    foto: '../img/Pilotos/Sebastien-Buemi.png',
+  },
+  {
+    nome: 'Nico Müller',
+    equipe: EQUIPES.abt,
+    foto: '../img/Pilotos/Nico-Muller.png',
+  },
+  {
+    nome: 'Edoardo Mortara',
+    equipe: EQUIPES.mahindra,
+    foto: '../img/Pilotos/Edoardo-Motara.png',
+  },
+  {
+    nome: 'Dan Ticktum',
+    equipe: EQUIPES.ert,
+    foto: '../img/Pilotos/Dan-Ticktum.png',
+  },
+  {
+    nome: 'Nyck de Vries',
+    equipe: EQUIPES.mahindra,
+    foto: '../img/Pilotos/Nyck-De-Vries.png',
+  },
+  {
+    nome: 'Norman Nato',
+    equipe: EQUIPES.andretti,
+    foto: '../img/Pilotos/Norman-Nato.png',
+  },
+  {
+    nome: 'Stoffel Vandoorne',
+    equipe: EQUIPES.dsPenske,
+    foto: '../img/Pilotos/Stoffel-Vandoorne.png',
+  },
+  {
+    nome: 'Sacha Fenestraz',
+    equipe: EQUIPES.nissan,
+    foto: '../img/Pilotos/Sacha-Fenestraz.png',
+  },
+  {
+    nome: 'Maximilian Günther',
+    equipe: EQUIPES.maseratti,
+    foto: '../img/Pilotos/Maximilian-Gunther.png',
+  },
+];
